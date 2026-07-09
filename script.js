d2ecc9b504e567b41646a33f94972975c330ee5d
Princy <princysiwach4@gmail.com>
2026-07-09 18:18:13 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,51 @@
+const words=[
+"AI Enthusiast",
+"Web Developer",
+"Tech Learner",
+"Problem Solver"
+];
+
+let i=0;
+let j=0;
+let current="";
+let isDeleting=false;
+
+function type(){
+
+current=words[i];
+
+if(isDeleting){
+
+document.getElementById("typing").textContent=current.substring(0,j--);
+
+}else{
+
+document.getElementById("typing").textContent=current.substring(0,j++);
+
+}
+
+if(!isDeleting && j===current.length+1){
+
+isDeleting=true;
+
+setTimeout(type,1200);
+
+return;
+
+}
+
+if(isDeleting && j===0){
+
+isDeleting=false;
+
+i++;
+
+if(i===words.length)i=0;
+
+}
+
+setTimeout(type,isDeleting?60:120);
+
+}
+
+type();
